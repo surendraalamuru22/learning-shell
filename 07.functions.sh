2026c1aae8370fba58e92eea584557b4a8e6a87f
surendra reddy <surendrareddylamuru01@gmail.com>
2024-02-09 15:13:27 +0530

    functions.sh

--- 07.functions.sh
+++ 07.functions.sh
@@ -1,6 +1,9 @@
 function SAMPLE() {
   echo a from main program = $a
+  a=200
+  echo a in funtion after override in function - $a
   b=10
+  echo first argument to script = $1
 
   echo SAMPLE FUNCTION
 }
@@ -13,9 +16,11 @@ SAMPLE1() {
 
 # main program
 
-SAMPLE
-SAMPLE1
+SAMPLE angel
+#SAMPLE1
 
 a=100
 echo b from function = $b
+b=200
+echo b from function after override in main program - $b
 
