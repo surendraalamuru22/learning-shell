9c59f3bb84e10b26de72c2c8e30693fd5c5c1839
surendra reddy <surendrareddylamuru01@gmail.com>
2024-02-13 10:09:05 +0530

    loops.sh

--- 08.loops.sh
+++ 08.loops.sh
@@ -14,18 +14,26 @@
 #  sleep 1
 #done
 
-for fruit in apple banana orange ; do
-  echo fruit name - $fruit
-  sleep 1
-done
+#for fruit in apple banana orange ; do
+#  echo fruit name - $fruit
+#  sleep 1
+#done
 
-a=10
+#a=10
 
-while [ $a -gt 0 ]; do
-  if [ $a == 5 ]; then
-    break
-    fi
-  echo hello - $a
-  a=$(($a-1))
-  sleep 1
+#while [ $a -gt 0 ]; do
+#  if [ $a == 5 ]; then
+#    break
+#    fi
+#  echo hello - $a
+#  a=$(($a-1))
+#  sleep 1
+#done
+
+for a in 1 2 3 4 5 6 7 8 9 ; do
+    if [ $a == 5 ]; then
+      break
+      fi
+      echo $a
+      sleep 1
 done
