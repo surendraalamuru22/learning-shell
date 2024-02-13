774b169eba3c0a4e5d8175c31b7c6b831378d97b
surendra reddy <surendrareddylamuru01@gmail.com>
2024-02-13 09:47:31 +0530

    loops.sh

--- 08.loops.sh
+++ 08.loops.sh
@@ -3,6 +3,7 @@ a=0
 while [ $a -lt 10 ]; do
   echo hello - $a
   a=$(($a+1))
+  sleep 1
 done
 
 a=10
@@ -10,4 +11,5 @@ a=10
 while [ $a -gt 0 ]; do
   echo hello - $a
   a=$(($a-1))
+  sleep 1
 done
